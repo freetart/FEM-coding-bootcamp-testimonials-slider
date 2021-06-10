85ee8f3aea50e8f4bc281fd5a24bbfe15972da0e
habib <habibdev96@gmail.com>
2021-06-09 18:07:04 -0600

    configured glidejs setup & created base html

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-"use scrict";
+import Glide from "@glidejs/glide";
+
+const quotesCarousel = () => {
+  const quotesCarouselConfig = {
+    type: "carousel",
+    autoplay: 5000,
+    perView: 1,
+  };
+
+  new Glide(".glide", quotesCarouselConfig).mount();
+};
+quotesCarousel();
